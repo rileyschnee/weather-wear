9e6e6445a07d9d3671e246192c8bbf33bcdbf53b
sfarishta <sahilfarishta@yahoo.com>
2019-10-31 21:39:58 -0400

    Added session ended request

--- index.js
+++ index.js
@@ -67,3 +67,13 @@ const ExitHandler = {
   },
 };
 
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+}
