ac82228a323c55a4efe923ae0a79eb50070a7602
Riley Schnee <rischnee@umich.edu>
2019-10-31 21:11:39 -0400

    Modifying to include check for valid geolocation

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ const LaunchRequestHandler = {
     },
     handle(handlerInput) {
         const speakOutput = 'Hello! Welcome to weather wear!';
+        var isGeoSupported = context.System.device.supportedInterfaces.Geolocation;
+        var geoObject = context.Geolocation;
+        if (isGeoSupported) {
+            var ACCURACY_THRESHOLD = 100; // accuracy of 100 meters required
+            if (geoObject && geoObject.coordinate && 
+                geoObject.coordinate.accuracyInMeters < ACCURACY_THRESHOLD ) { 
+                // TODO: get the weather
+                console.log(geoObject);  // Print the geo-coordinates object if accuracy is within 100 meters
+            }
+        } else {
+            speakOutput = 'I am experiencing issues connecting to your location.';
+        }
         return handlerInput.responseBuilder
             .speak(speakOutput)
             // .reprompt(speakOutput)
             .getResponse();
     }
 };
+
